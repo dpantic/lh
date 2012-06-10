36c1c34a877fa680bb4f49564593a7af8cea2997
dpantic <dpantic@gmail.com>
2012-06-10 12:52:04 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lufthansa::Application.config.secret_token = '66738dd8c439756cc076b94ab85178d2ee334c0028d9033bb5d7c8e142022718196fae14c0efac53e0667c34e34df5d5955e051f61cdd0577f9cebb510b9e571'
